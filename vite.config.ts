cf4cf8d2eacfb41ca875bdb20d3c98b486187eda
Kylver21 <kilverpaucar1@gmail.com>
2025-06-16 22:23:44 -0500

    correccion de errores

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/control-gastos/', // <--- importante
+  base: '/control-gastos/', // Debe coincidir con tu repo
   plugins: [react()],
 })
