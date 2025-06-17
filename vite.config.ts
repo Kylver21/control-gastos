18df2b3aacdeb2a0d3e28591bf4e9df3b9b67f72
Kylver21 <kilverpaucar1@gmail.com>
2025-06-16 22:35:02 -0500

    otros dispositivos

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/control-gastos/', // Debe coincidir con tu repo
+  base: '/control-gastos/', // Ya está correcto
   plugins: [react()],
 })
